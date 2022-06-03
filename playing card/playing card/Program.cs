921dbcbd5a05e345ab9505bde79195c7c6b2387a
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2022-06-03 19:56:45 +0300

    Add files via upload

---
+++ playing card/playing card/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using playing_card.Cards;
+
+namespace playing_card
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck.FillDeck();
+         //   Deck.PrintDeck();
+            Deck.Shuffle();
+            Deck.PrintDeck();
+            Console.ReadLine();
+        }
+    }
+}
